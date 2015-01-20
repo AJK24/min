2435cfcf0775fea8cbb313fffb43ff1e63b5fa82
andrew <andrew901@sjccs142.saintjoe.edu>
2015-01-20 17:14:06 -0500

    This is a creation of the originial min printer ~

---
+++ min.js
@@ -0,0 +1,14 @@
+var min = function (x,y){
+       if(x<y){
+         return x;
+       else//if x and y are the same the same number will also be printed
+	 return y;
+}
+
+
+ console.log(min(0, 10));
+// // -> 0
+console.log(min(0, 0));
+// 0
+ console.log(min(0, -10));
+// // -> -10
